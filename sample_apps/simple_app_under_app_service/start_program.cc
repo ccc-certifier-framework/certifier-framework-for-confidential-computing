b7689b06174708eec5718d7d522dedd960009754
John Manferdelli <johnmanferdelli@hotmail.com>
2022-08-05 13:01:03 -0700

    fixed start_program

--- sample_apps/simple_app_under_app_service/start_program.cc
+++ sample_apps/simple_app_under_app_service/start_program.cc
@@ -12,6 +12,40 @@ DEFINE_int32(server_app_port, 8127, "port for application requests");
 DEFINE_string(executable, "service_example_app.exe", "executable to run");
 DEFINE_string(args, "service_example_app.exe", "service example arguments");
 
+#define DEBUG
+
+
+bool parse_args(const string& in, int* num_an, string* av) {
+  // for now, just assume commas can only be delimiters
+  const char* start = in.c_str();
+  const char* end = start;
+
+  *num_an = 0;
+  while (end != nullptr) {
+    if (*end == ',' || *end == '\0') {
+      av[*num_an].assign(start, end - start);
+      av[*num_an].append('\0', 1);
+      (*num_an)++;
+      if (*end == '\0')
+        return true;
+      end++;
+      start = end;
+    }
+    end++;
+  }
+
+  return true;
+}
+
+void print_run_request(run_request& r) {
+  if (r.has_location()) {
+    printf("Executable: %s\n", r.location().c_str());
+  }
+  for (int i = 0; i < r.args_size(); i++) {
+    printf("  arg[%d]: %s\n", i, r.args(i).c_str());
+  }
+}
+
 int main(int an, char**av) {
   gflags::ParseCommandLineFlags(&an, &av, true);
   an = 1;
@@ -19,7 +53,33 @@ int main(int an, char**av) {
   run_request req;
   run_response rsp;
 
- // dial service
+
+  // Set executable
+  printf("Executable: %s\n", FLAGS_executable.c_str());
+  req.set_location(FLAGS_executable);
+
+  // Set flags
+  const int max_args= 15;
+  int num_args = max_args;
+  string s_args[max_args];
+  if (!parse_args(FLAGS_args, &num_args, s_args)) {
+    printf("Can't parse argument list\n");
+    num_args = 0;
+  }
+
+  // req.args
+  for (int i = 0; i < num_args; i++) {
+    string *n_a = req.add_args();
+    n_a->assign(s_args[i]);
+  }
+
+#ifdef DEBUG
+  printf("\n%d args\n", num_args);
+  print_run_request(req);
+  printf("\n");
+#endif
+
+  // dial service
   struct sockaddr_in address;
   memset((byte*)&address, 0, sizeof(struct sockaddr_in));
   int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,13 +97,11 @@ int main(int an, char**av) {
     return 1;
   }
 
-  printf("Executable: %s\n", FLAGS_executable.c_str());
-  req.set_location(FLAGS_executable);
+  // write request
   string serialized_request;
   if (!req.SerializeToString(&serialized_request)) {
     return 1;
   }
-  // write request
   if (write(sock, (byte*)serialized_request.data(), serialized_request.size()) < 0) {
     return 1;
   }
