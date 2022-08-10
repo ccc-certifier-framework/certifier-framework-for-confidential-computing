edb56e4f554a2333d70fed55fe7555cc52123ec5
Anoop Jaishankar <ajaishankar@vmware.com>
2022-08-10 15:26:47 -0700

    Certifier support for both GRPC server and client

---
+++ asylo/asylo/examples/secure_grpc/grpc_client_enclave.cc
@@ -0,0 +1,159 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "asylo/examples/secure_grpc/grpc_client_enclave.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "absl/status/status.h"
+#include "absl/time/time.h"
+#include "asylo/enclave.pb.h"
+#include "asylo/examples/grpc_server/translator_server.grpc.pb.h"
+#include "asylo/examples/secure_grpc/grpc_client_enclave.pb.h"
+#include "asylo/grpc/auth/enclave_channel_credentials.h"
+#include "asylo/grpc/auth/sgx_local_credentials_options.h"
+#include "asylo/trusted_application.h"
+#include "asylo/util/status.h"
+#include "asylo/util/status_macros.h"
+#include "asylo/util/statusor.h"
+#include "include/grpc/impl/codegen/gpr_types.h"
+#include "include/grpc/support/time.h"
+#include "include/grpcpp/grpcpp.h"
+
+extern bool certifier_init(char* usr_data_dir, size_t usr_data_dir_size);
+extern bool cold_init();
+extern bool certify_me();
+extern bool setup_client_ssl();
+
+namespace examples {
+namespace secure_grpc {
+namespace {
+
+using grpc_server::Translator;
+
+const absl::Duration kChannelDeadline = absl::Seconds(5);
+
+// Makes a GetTranslation RPC with |request| to the server backed by *|stub|.
+asylo::StatusOr<grpc_server::GetTranslationResponse> GetTranslation(
+    const grpc_server::GetTranslationRequest &request, Translator::Stub *stub) {
+  grpc_server::GetTranslationResponse response;
+
+  ::grpc::ClientContext context;
+  ASYLO_RETURN_IF_ERROR(
+      asylo::Status(stub->GetTranslation(&context, request, &response)));
+  return response;
+}
+
+}  // namespace
+
+asylo::Status GrpcClientEnclave::Initialize(
+    const asylo::EnclaveConfig &enclave_config) {
+  std::string data_dir = "./client_data";
+
+  LOG(INFO) << "Client initializing";
+
+  bool cert_result = false;
+  cert_result = certifier_init((char*)data_dir.c_str(), data_dir.size());
+  LOG_IF(QFATAL, !cert_result)
+      << "certifier_init failed: result = " << cert_result;
+
+  cert_result = cold_init();
+  LOG_IF(QFATAL, !cert_result)
+      << "cold_init failed: result = " << cert_result;
+
+  cert_result = certify_me();
+  LOG_IF(QFATAL, !cert_result)
+      << "certify_me failed: result = " << cert_result;
+
+  LOG(INFO) << "Client done initializing";
+
+  return absl::OkStatus();
+}
+
+asylo::Status GrpcClientEnclave::Run(const asylo::EnclaveInput &input,
+                                     asylo::EnclaveOutput *output) {
+  bool cert_result = false;
+  LOG(INFO) << "Client requesting ssl check";
+
+  cert_result = setup_client_ssl();
+  LOG_IF(QFATAL, !cert_result)
+      << "setup_ssl failed: result = " << cert_result;
+
+  LOG(INFO) << "Client done ssl check";
+
+  if (!input.HasExtension(client_enclave_input)) {
+    return absl::InvalidArgumentError("Input missing client_input extension");
+  }
+  const GrpcClientEnclaveInput &client_input =
+      input.GetExtension(client_enclave_input);
+
+  const std::string &address = client_input.server_address();
+  if (address.empty()) {
+    return absl::InvalidArgumentError(
+        "Input must provide a non-empty server address");
+  }
+  if (client_input.translation_request().input_word().empty()) {
+    return absl::InvalidArgumentError(
+        "Input must provide a non-empty RPC input");
+  }
+
+  // The ::grpc::ChannelCredentials object configures the channel authentication
+  // mechanisms used by the client and server. This particular configuration
+  // enforces that both the client and server authenticate using SGX local
+  // attestation.
+  std::shared_ptr<::grpc::ChannelCredentials> channel_credentials =
+      EnclaveChannelCredentials(
+          asylo::BidirectionalSgxLocalCredentialsOptions());
+
+  // Connect a gRPC channel to the server specified in the EnclaveInput.
+  std::shared_ptr<::grpc::Channel> channel =
+      ::grpc::CreateChannel(address, channel_credentials);
+
+  gpr_timespec absolute_deadline = gpr_time_add(
+      gpr_now(GPR_CLOCK_REALTIME),
+      gpr_time_from_micros(absl::ToInt64Microseconds(kChannelDeadline),
+                           GPR_TIMESPAN));
+  if (!channel->WaitForConnected(absolute_deadline)) {
+    return absl::InternalError("Failed to connect to server");
+  }
+
+  GrpcClientEnclaveOutput *client_output =
+      output->MutableExtension(client_enclave_output);
+
+  std::unique_ptr<Translator::Stub> stub = Translator::NewStub(channel);
+  ASYLO_ASSIGN_OR_RETURN(
+      *client_output->mutable_translation_response(),
+      GetTranslation(client_input.translation_request(), stub.get()));
+
+  return absl::OkStatus();
+}
+
+}  // namespace secure_grpc
+}  // namespace examples
+
+namespace asylo {
+
+// Registers an instance of GrpcClientEnclave as the TrustedApplication. See
+// trusted_application.h for more information.
+TrustedApplication *BuildTrustedApplication() {
+  return new examples::secure_grpc::GrpcClientEnclave();
+}
+
+}  // namespace asylo
---
+++ asylo/asylo/examples/secure_grpc/grpc_server_main.cc
@@ -0,0 +1,83 @@
+/*
+ *
+ * Copyright 2019 Asylo authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+#include <google/protobuf/text_format.h>
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/time/clock.h"
+#include "asylo/enclave_manager.h"
+#include "asylo/examples/secure_grpc/grpc_server_util.h"
+#include "asylo/util/logging.h"
+#include "asylo/identity/platform/sgx/sgx_identity.pb.h"
+#include "asylo/util/proto_flag.h"
+#include "asylo/util/status.h"
+#include "asylo/util/statusor.h"
+
+ABSL_FLAG(std::string, enclave_path, "", "Path to enclave to load");
+
+// By default, let the server run for five minutes.
+ABSL_FLAG(int32_t, server_max_lifetime, 300,
+          "The longest amount of time (in seconds) that the server should be "
+          "allowed to run");
+
+// Default value 0 is used to indicate that the system should choose an
+// available port.
+//ABSL_FLAG(int32_t, port, 0, "Port that the server listens to");
+ABSL_FLAG(int32_t, port, 32455, "Port that the server listens to");
+ABSL_FLAG(bool, debug, true, "Whether to use a debug enclave");
+
+ABSL_FLAG(asylo::SgxIdentityExpectation, acl, {},
+          "The ACL to enforce within the server");
+
+int main(int argc, char *argv[]) {
+  // Parse command-line arguments.
+  absl::ParseCommandLine(argc, argv);
+
+  std::string enclave_path = absl::GetFlag(FLAGS_enclave_path);
+  LOG_IF(QFATAL, enclave_path.empty()) << "--enclave_path cannot be empty";
+
+  asylo::Status status =
+      asylo::EnclaveManager::Configure(asylo::EnclaveManagerOptions());
+  LOG_IF(QFATAL, !status.ok())
+      << "Failed to configure EnclaveManager: " << status;
+
+  status = examples::secure_grpc::LoadGrpcServerEnclave(
+      enclave_path, absl::GetFlag(FLAGS_port), absl::GetFlag(FLAGS_acl),
+      absl::GetFlag(FLAGS_debug));
+  LOG_IF(QFATAL, !status.ok())
+      << "Loading " << enclave_path << " failed: " << status;
+
+  asylo::StatusOr<int> port_result =
+      examples::secure_grpc::GrpcServerEnclaveGetPort();
+  LOG_IF(QFATAL, !port_result.ok())
+      << "Retrieving port failed: " << port_result.status();
+
+  std::cout << "Server started on port " << port_result.value() << std::endl;
+
+  absl::SleepFor(absl::Seconds(absl::GetFlag(FLAGS_server_max_lifetime)));
+
+  status = examples::secure_grpc::DestroyGrpcServerEnclave();
+  LOG_IF(QFATAL, !status.ok())
+      << "Destroy " << enclave_path << " failed: " << status;
+
+  return 0;
+}
