70c3a7d4357ffd1d96010fd544ecc2330b75eec9
John Manferdelli <johnmanferdelli@hotmail.com>
2022-09-26 12:33:16 -0700

    one more

--- certifier_service/simpleserver.go
+++ certifier_service/simpleserver.go
@@ -1,4 +1,4 @@
-//  Copyright (c) 2021-22, VMware Inc, and the Certifier Authors.  All rights reserved.
+ereport/  Copyright (c) 2021-22, VMware Inc, and the Certifier Authors.  All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -943,6 +943,8 @@ func ConstructProofFromRequest(evidenceType string, support *certprotos.Evidence
                         fmt.Printf("Signed report\n")
                 } else if support.FactAssertion[i].GetEvidenceType() == "cert" {
                         fmt.Printf("Cert\n")
+                } else if support.FactAssertion[i].GetEvidenceType() == "sev-attestation" {
+                        fmt.Printf("sev-attestation\n")
                 } else {
                         fmt.Printf("Invalid evidence type\n")
                         return nil, nil, nil
