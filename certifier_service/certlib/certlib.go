f81a1b3c25493022575d14a58a642de445bb24a6
John Manferdelli <johnmanferdelli@hotmail.com>
2022-10-02 13:45:28 -0700

    move sig

--- certifier_service/certlib/certlib.go
+++ certifier_service/certlib/certlib.go
@@ -1655,8 +1655,8 @@ func VerifySevAttestation(serialized []byte, k *certprotos.KeyMessage) []byte {
 	fmt.Printf("VerifySevAttestation, vcekKey: ")
 	PrintKey(k)
 	fmt.Printf("\n")
-	fmt.Printf("VerifySevAttestation, report: ")
-	PrintBytes(ptr[0:0x360])
+	fmt.Printf("VerifySevAttestation, report (%d): ", len(am.WhatWasSaid))
+	PrintBytes(ptr[0:0x330])
 	fmt.Printf("\n")
 	fmt.Printf("VerifySevAttestation, Header of report: ")
 	PrintBytes(ptr[0:0x2a0])
@@ -1673,7 +1673,8 @@ func VerifySevAttestation(serialized []byte, k *certprotos.KeyMessage) []byte {
 	fmt.Printf("\n")
 
 	r :=  new(big.Int).SetBytes(ptr[0x2a0:0x2d0])
-	s :=  new(big.Int).SetBytes(ptr[0x2d0:0x300])
+	// s :=  new(big.Int).SetBytes(ptr[0x2d0:0x300])
+	s :=  new(big.Int).SetBytes(ptr[0x300:0x330])
 	if !ecdsa.Verify(PK, hashOfHeader[0:48], r, s) {
 		fmt.Printf("VerifySevAttestation: ecdsa.Verify failed\n")
 		return nil
