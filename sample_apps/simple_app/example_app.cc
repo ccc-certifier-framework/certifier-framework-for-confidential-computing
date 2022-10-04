16e49c4bbc83f96207720c6d1e501ff30a06464e
John Manferdelli <johnmanferdelli@hotmail.com>
2022-10-04 13:02:13 -0700

    Removed cert printing

--- sample_apps/simple_app/example_app.cc
+++ sample_apps/simple_app/example_app.cc
@@ -68,7 +68,9 @@ void client_application(secure_authenticated_channel& channel) {
   printf("Client peer id is %s\n", channel.peer_id_.c_str());
   if (channel.peer_cert_ != nullptr) {
     printf("Client peer cert is:\n");
+#ifdef DEBUG
     X509_print_fp(stdout, channel.peer_cert_);
+#endif
   }
 
   // client sends a message over authenticated, encrypted channel
@@ -87,7 +89,9 @@ void server_application(secure_authenticated_channel& channel) {
   printf("Server peer id is %s\n", channel.peer_id_.c_str());
   if (channel.peer_cert_ != nullptr) {
     printf("Server peer cert is:\n");
+#ifdef DEBUG
     X509_print_fp(stdout, channel.peer_cert_);
+#endif
   }
 
   // Read message from client over authenticated, encrypted channel
