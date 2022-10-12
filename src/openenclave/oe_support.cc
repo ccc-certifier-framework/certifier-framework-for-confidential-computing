2d4cf157ee420b0aca61a97e6e072f06c1503266
John Manferdelli <johnmanferdelli@hotmail.com>
2022-10-12 11:35:34 -0700

    undefined vars

--- src/openenclave/oe_support.cc
+++ src/openenclave/oe_support.cc
@@ -11,13 +11,15 @@ string pem_cert_chain;
 bool pem_cert_chain_initialized = false;
 
 bool oe_Init(const string& pem_cert_chain_file) {
+  extern bool certifier_parent_enclave_type_intitalized;
+  extern string certifier_parent_enclave_type; 
    if (!read_file_into_string(pem_cert_chain_file, &pem_cert_chain)) {
     printf("oe_Init: Can't read pem cert chain file\n");
     return false;
   }
 
   pem_cert_chain_initialized = true;
-  // certifier_parent_enclave_type = "hardware";
-  // certifier_parent_enclave_type_intitalized = true;
+  certifier_parent_enclave_type = "hardware";
+  certifier_parent_enclave_type_intitalized = true;
   return true;
 }
