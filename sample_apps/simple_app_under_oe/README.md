861bef128fbbc6ac7a5540cbcb24849cfb974726
Ye Li <yel@vmware.com>
2022-10-12 14:48:12 -0400

    Added OE simple app template.

---
+++ sample_apps/simple_app_under_oe/README.md
@@ -0,0 +1,175 @@
+
+# Privacy Preserving data analytics with Certifier Framework 
+
+This sample application demonstrates how to conduct privacy preserving data analytics with 
+the Certifier Framework on openenclave. It has the following properties: 
+* Demonstrates how to securely use certifier framework  
+   * configures custom trusted policy and security domain
+   * conducts data analytics only on certified secure enclaves  
+* Deploys an existing openenclave application with the certifier framework
+* End-to-end privacy preserving data analytics 
+   * protects the dataset sent to the enclave
+   * protects the result returned from the enclave
+   * secures the analytics computation done in the enclave
+
+**Note:** 
+The existing demostration has been tested with SGX hardware mode and simulation mode, but it 
+does not leverage the openenclave's interface for getting and verifying the measurement.
+Future release will fix this issue.  
+
+### On Simulated Enclave 
+
+#### Prerequisites 
+The example is build on [Open Enclave](https://github.com/openenclave/openenclave). 
+Please refer to the [link](https://github.com/openenclave/openenclave/tree/master/docs/GettingStartedDocs#hardware-drivers)
+ for instructions. The example also relies on the protobuf built specifically with 
+ openenclave libc, which the instructions can be found [here](../../openenclave_test/instructions.txt)
+
+To configure the environment variables for this project, run 
+```
+export CERTIFIER=~/certifier-github-mirror
+export CERTIFIER_PROTOTYPE=$CERTIFIER
+export EXAMPLE_DIR=$CERTIFIER_PROTOTYPE/sample_apps/analytics_example
+export PATH=$PATH:/usr/local/go/bin && export PATH=$PATH:$(go env GOPATH)/bin
+source /opt/openenclave/share/openenclave/openenclaverc
+```
+`$CERTIFIER` is the top level directory for the certifier repository.
+
+#### Build and Run
+
+Step 1: Load the third party libraries 
+```bash
+cd $EXAMPLE_DIR/third_party
+chmod +x ./load_dataframe.sh
+./load_dataframe.sh
+```
+This will load and build the [Dataframe](https://github.com/hosseinmoein/DataFrame)
+dependency of the project with openenclave SDK.
+
+Step 1: Build the utilities
+```bash
+cd $CERTIFIER_PROTOTYPE
+cd utilities
+make -f cert_utility.mak
+make -f policy_utilities.mak
+```
+
+
+Step 2: Generate the policy key and self-signed cert
+```bash
+mkdir $EXAMPLE_DIR/provisioning
+cd $EXAMPLE_DIR/provisioning
+$CERTIFIER_PROTOTYPE/utilities/cert_utility.exe --operation=generate-policy-key-and-test-keys \
+    --policy_key_output_file=policy_key_file.bin --policy_cert_output_file=policy_cert_file.bin \
+    --platform_key_output_file=platform_key_file.bin --attest_key_output_file=attest_key_file.bin
+```
+This will also generate the attestation key and platform key for the these tests.
+
+Step 3: Embed the policy key in example_app.
+```bash
+cd $EXAMPLE_DIR/provisioning
+$CERTIFIER_PROTOTYPE/utilities/embed_policy_key.exe --input=policy_cert_file.bin --output=../policy_key.cc
+```
+
+Step 4: Compile example_app with the embedded policy_key
+```bash
+cd $EXAMPLE_DIR
+make
+```
+
+Step 6: Obtain the measurement of the trusted application for the security domain.
+```bash
+cd $EXAMPLE_DIR/provisioning
+$CERTIFIER_PROTOTYPE/utilities/measurement_utility.exe --type=hash --input=../enclave/enclave.signed \
+      --output=example_app.measurement
+```
+
+Step 7: Author the policy for the security domain and produce the signed claims the apps need.
+```bash
+cd $EXAMPLE_DIR/provisioning
+
+# a. Construct statement "policy-key says example_app-measurement is-trusted"
+$CERTIFIER_PROTOTYPE/utilities/make_unary_vse_clause.exe --key_subject="" \
+   --measurement_subject=example_app.measurement --verb="is-trusted" \
+   --output=ts1.bin
+$CERTIFIER_PROTOTYPE/utilities/make_indirect_vse_clause.exe --key_subject=policy_key_file.bin \
+   --verb="says" --clause=ts1.bin --output=vse_policy1.bin
+
+# b. Construct statement "policy-key says the platform-key is-trusted-for-attestation"
+$CERTIFIER_PROTOTYPE/utilities/make_unary_vse_clause.exe --key_subject=platform_key_file.bin \
+   --verb="is-trusted-for-attestation" --output=ts2.bin
+$CERTIFIER_PROTOTYPE/utilities/make_indirect_vse_clause.exe --key_subject=policy_key_file.bin \
+   --verb="says" --clause=ts2.bin --output=vse_policy2.bin
+
+# c. Produce the signed claims for each vse policy statement.
+$CERTIFIER_PROTOTYPE/utilities/make_signed_claim_from_vse_clause.exe \
+   --vse_file=vse_policy1.bin --duration=9000 \
+   --private_key_file=policy_key_file.bin --output=signed_claim_1.bin
+$CERTIFIER_PROTOTYPE/utilities/make_signed_claim_from_vse_clause.exe --vse_file=vse_policy2.bin \
+   --duration=9000 --private_key_file=policy_key_file.bin --output=signed_claim_2.bin
+
+# d. Combine signed policy statements for Certifier Service use.
+$CERTIFIER_PROTOTYPE/utilities/package_claims.exe --input=signed_claim_1.bin,signed_claim_2.bin\
+   --output=policy.bin
+
+# e. Construct statement "platform-key says attestation-key is-trusted-for-attestation" and sign it
+$CERTIFIER_PROTOTYPE/utilities/make_unary_vse_clause.exe --key_subject=attest_key_file.bin \
+   --verb="is-trusted-for-attestation" --output=tsc1.bin
+$CERTIFIER_PROTOTYPE/utilities/make_indirect_vse_clause.exe --key_subject=platform_key_file.bin \
+   --verb="says" --clause=tsc1.bin --output=vse_policy3.bin
+$CERTIFIER_PROTOTYPE/utilities/make_signed_claim_from_vse_clause.exe --vse_file=vse_policy3.bin \
+   --duration=9000 --private_key_file=platform_key_file.bin \
+   --output=platform_attest_endorsement.bin
+```
+
+Step 8: Provisioning data 
+```bash
+cd $EXAMPLE_DIR
+mkdir app1_data app2_data service
+cd $EXAMPLE_DIR/provisioning
+cp ./* $EXAMPLE_DIR/app1_data
+cp ./* $EXAMPLE_DIR/app2_data
+cp policy_key_file.bin policy_cert_file.bin policy.bin $EXAMPLE_DIR/service
+```
+
+Step 9: Start the Certifier Service
+  In a new terminal window:
+```bash
+cd $EXAMPLE_DIR/service
+$CERTIFIER_PROTOTYPE/certifier_service/simpleserver \
+--path=$EXAMPLE_DIR/service \
+--policyFile=policy.bin --readPolicy=true
+```
+
+Step 10:  Run the apps and get admission certificates from Certifier Service
+  Open two new terminals (one for the app as a client and one for the app as a server):
+The `--simulate` flag is only required for running the application in SGX simulation mode.
+
+On the client's terminal, run 
+```bash
+cd $EXAMPLE_DIR
+./host/host ./enclave/enclave.signed cold-init $EXAMPLE_DIR/app1_data --simulate
+./host/host ./enclave/enclave.signed get-certifier $EXAMPLE_DIR/app1_data --simulate
+```
+
+On the server's terminal, run 
+```bash
+cd $EXAMPLE_DIR
+./host/host ./enclave/enclave.signed cold-init $EXAMPLE_DIR/app2_data --simulate
+./host/host ./enclave/enclave.signed get-certifier $EXAMPLE_DIR/app2_data/ --simulate
+```
+
+Step 11:  Run the apps to test trusted services
+
+Run the data analytics server as 
+```bash
+cd $EXAMPLE_DIR
+./host/host ./enclave/enclave.signed run-app-as-server $EXAMPLE_DIR/app1_data --simulate
+```
+Run the data analytics client as 
+```bash
+cd $EXAMPLE_DIR
+./host/host ./enclave/enclave.signed run-app-as-client $EXAMPLE_DIR/app1_data --simulate
+```
+You should be able to see the server process the dataset provided by the client. 
+
---
+++ sample_apps/simple_app_under_oe/enclave/ecalls.cc
@@ -0,0 +1,285 @@
+#include <stdio.h>
+#include <sys/mount.h>
+#include <openssl/rand.h>
+#include <openenclave/enclave.h>
+#include <openenclave/attestation/attester.h>
+#include <openenclave/attestation/custom_claims.h>
+#include <openenclave/attestation/verifier.h>
+#include <openenclave/bits/report.h>
+#include <openenclave/bits/module.h>
+#include "certifier.h"
+#include "support.h"
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include  <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/rsa.h>
+#include <openssl/x509.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/hmac.h>
+#include <openssl/err.h>
+
+#include "certifier.h"
+#include "support.h"
+#include "cc_helpers.h"
+#include "../policy_key.cc"
+
+#define FLAGS_print_all true
+#define FLAGS_operation ""
+#define FLAGS_policy_host "localhost"
+#define FLAGS_policy_port 8123
+#define FLAGS_server_app_host "localhost"
+#define FLAGS_server_app_port 8124
+static string data_dir = "app1_data";
+
+#define FLAGS_policy_store_file "store.bin"
+#define FLAGS_certificate_file "vse.crt"
+
+static std::string enclave_type; 
+cc_trust_data* app_trust_data = nullptr;
+
+static bool oe_initialized = false;
+static bool openenclave_initialized = false;
+bool test_local_certify(string& enclave_type,
+       bool init_from_file, string& file_name,
+       string& evidence_descriptor);
+
+
+bool trust_data_initialized = false;
+key_message privatePolicyKey;
+key_message publicPolicyKey;
+string serializedPolicyCert;
+X509* policy_cert= nullptr;
+
+policy_store pStore;
+key_message privateAppKey;
+key_message publicAppKey;
+const int app_symmetric_key_size = 64;
+byte app_symmetric_key[app_symmetric_key_size];
+key_message symmertic_key_for_protect;
+
+// Standard algorithms for the enclave
+string public_key_alg("rsa-2048");
+string symmetric_key_alg("aes-256");
+string hash_alg("sha-256");
+string hmac_alg("sha-256-hmac");
+
+void print_trust_data() {
+  if (!trust_data_initialized)
+    return;
+  printf("\nTrust data:\n");
+  printf("\nPolicy key\n");
+  print_key(publicPolicyKey);
+  printf("\nPolicy cert\n");
+  print_bytes(serializedPolicyCert.size(), (byte*)serializedPolicyCert.data());
+  printf("\n");
+  printf("\nPrivate app auth key\n");
+  print_key(privateAppKey);
+  printf("\nPublic app auth key\n");
+  print_key(publicAppKey);
+  printf("\nBlob key\n");
+  print_key(symmertic_key_for_protect);
+  printf("\n\n");
+}
+
+extern "C"
+{
+  bool openenclave_init(void);
+  bool certifier_init(char*, size_t);
+
+  bool cold_init(void);
+  bool certify_me(void);
+  bool warm_restart(void);
+  bool run_me_as_client(void);
+  bool run_me_as_server(void);
+}
+
+bool openenclave_init(void){
+  oe_result_t result = OE_OK;
+  result = oe_load_module_host_file_system();
+  if (result != OE_OK) {
+      printf("Failed to load host file system module: %s\n", oe_result_str(result));
+      return false;
+  }
+  if (mount("/", "/", OE_HOST_FILE_SYSTEM, 0, NULL) != 0) {
+      printf("Failed to mount host file system\n");
+      return false;
+  }
+
+  result = oe_load_module_host_socket_interface();
+    if (result != OE_OK) {
+      printf("Failed to load socket module: %s\n", oe_result_str(result));
+      return false;
+  }
+
+  result = oe_load_module_host_resolver();
+    if (result != OE_OK) {
+      printf("Failed to load resolver module: %s\n", oe_result_str(result));
+      return false;
+  }
+  openenclave_initialized = true;
+
+  return true;
+}
+
+bool certifier_init(char* usr_data_dir, size_t usr_data_dir_size) {
+  oe_result_t result = OE_OK;
+  static const char rnd_seed[] =
+    "string to make the random number generator think it has entropy";
+
+  RAND_seed(rnd_seed, sizeof rnd_seed);
+  std::string usr_data = usr_data_dir;
+  data_dir =  usr_data + "/";
+  printf("Using data_dir: %s\n", data_dir.c_str());
+
+  if(!openenclave_initialized){
+    openenclave_init();
+  }
+
+  // Initialize attester and use the plugin.
+  result = oe_attester_initialize();
+  if (result != OE_OK)
+  {
+      printf("oe_attester_initialize failed.\n");
+      return false;
+  }
+  // Initialize verifier and use the plugin.
+  result = oe_verifier_initialize();
+  if (result != OE_OK)
+  {
+      printf("oe_verifier_initialize failed.\n");
+      return false;
+  }
+
+  if (!oe_initialized) {
+    SSL_library_init();
+    string enclave_type("oe-enclave");
+    string purpose("authentication");
+
+    string store_file(data_dir);
+    store_file.append(FLAGS_policy_store_file);
+    app_trust_data = new cc_trust_data(enclave_type, purpose, store_file);
+    if (app_trust_data == nullptr) {
+      printf("couldn't initialize trust object\n");
+      return false;
+    }
+
+    // Init policy key info
+    if (!app_trust_data->init_policy_key(initialized_cert_size, initialized_cert)) {
+      printf("Can't init policy key\n");
+      return false;
+    }
+
+    string cert(FLAGS_certificate_file);
+    if (!app_trust_data->initialize_oe_enclave_data(cert)) {
+      printf("Can't init OE enclave\n");
+      return false;
+    }
+
+    oe_initialized = true;
+  }
+
+  return true;
+}
+
+void clear_sensitive_data() {
+  // Todo: clear symmetric and private keys
+  //    Not necessary on most platforms
+}
+
+bool cold_init() {
+  return app_trust_data->cold_init(public_key_alg, symmetric_key_alg,
+                                   hash_alg, hmac_alg);
+}
+
+bool warm_restart() {
+  return app_trust_data->warm_restart();
+}
+
+// Todo: replace with new cc_trust_data interface
+bool certify_me() {
+  return app_trust_data->certify_me(FLAGS_policy_host, FLAGS_policy_port);
+}
+
+void server_application(secure_authenticated_channel& channel) {
+
+  printf("Server peer id is %s\n", channel.peer_id_.c_str());
+  if (channel.peer_cert_ != nullptr) {
+    printf("Server peer cert is:\n");
+#ifdef DEBUG
+    X509_print_fp(stdout, channel.peer_cert_);
+#endif
+  }
+
+  // Read message from client over authenticated, encrypted channel
+  string out;
+  int n = channel.read(&out);
+  printf("SSL server read: %s\n", (const char*) out.data());
+
+  // Reply over authenticated, encrypted channel
+  const char* msg = "Hi from your secret server\n";
+  channel.write(strlen(msg), (byte*)msg);
+}
+
+bool run_me_as_server() {
+  if (!app_trust_data->warm_restart()) {
+    printf("warm-restart failed\n");
+    return false;
+  }
+  printf("running as server\n");
+  server_dispatch(FLAGS_server_app_host, FLAGS_server_app_port,
+      app_trust_data->serialized_policy_cert_,
+        app_trust_data->private_auth_key_,
+        app_trust_data->private_auth_key_.certificate(),
+        server_application);
+  return true;
+}
+
+void client_application(secure_authenticated_channel& channel) {
+
+  printf("Client peer id is %s\n", channel.peer_id_.c_str());
+  if (channel.peer_cert_ != nullptr) {
+    printf("Client peer cert is:\n");
+#ifdef DEBUG
+    X509_print_fp(stdout, channel.peer_cert_);
+#endif
+  }
+
+  // client sends a message over authenticated, encrypted channel
+  const char* msg = "Hi from your secret client\n";
+  channel.write(strlen(msg), (byte*)msg);
+
+  // Get server response over authenticated, encrypted channel and print it
+  string out;
+  int n = channel.read(&out);
+  printf("SSL client read: %s\n", out.data());
+}
+
+bool run_me_as_client() {
+  if (!app_trust_data->warm_restart()) {
+    printf("warm-restart failed\n");
+    return false;
+  }
+  printf("running as client\n");
+  if (!app_trust_data->cc_auth_key_initialized_ ||
+      !app_trust_data->cc_policy_info_initialized_) {
+    printf("trust data not initialized\n");
+    return false;
+  }
+  string my_role("client");
+  secure_authenticated_channel channel(my_role);
+  if (!channel.init_client_ssl(FLAGS_server_app_host, FLAGS_server_app_port,
+        app_trust_data->serialized_policy_cert_,
+        app_trust_data->private_auth_key_,
+        app_trust_data->private_auth_key_.certificate())) {
+    printf("Can't init client app\n");
+    return false;
+  }
+
+  // This is the actual application code.
+  client_application(channel);
+  return true;
+}
