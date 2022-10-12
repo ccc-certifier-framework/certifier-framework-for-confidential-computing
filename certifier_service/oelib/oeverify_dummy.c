80592848349f85a414ff35054661172e4ec97060
Ye Li <yel@vmware.com>
2022-10-11 22:18:21 -0400

    Add dummy OE library for non-OE build.

---
+++ certifier_service/oelib/oeverify_dummy.c
@@ -0,0 +1,15 @@
+#include "oeverify.h"
+
+bool oe_host_verify_evidence(
+    uint8_t* evidence,
+    size_t evidence_size,
+    uint8_t* endorsements,
+    size_t endorsements_size,
+    uint8_t* custom_claim_out,
+    size_t* custom_claim_size,
+    uint8_t* measurement_out,
+    size_t* measurement_size)
+{
+    fprintf(stderr, "Dummy oe_host_verify_evidence call.\n");
+    return false;
+}
