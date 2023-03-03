149f3b5e2276fd97a894e1e9f519fccd7bab9b1a
Anoop Jaishankar <ajaishankar@vmware.com>
2023-03-03 14:31:15 -0800

    Move more functions into certifier

--- src/gramine/gramine_api_impl.cc
+++ src/gramine/gramine_api_impl.cc
@@ -37,7 +37,15 @@
 
 #define MAX_ASSERTION_SIZE 5000
 
+#define BUF_SIZE 10
+#define TAG_SIZE 16
+#define KEY_SIZE 16
+
+#define SGX_QUOTE_SIZE 32
+
 uint8_t g_quote[SGX_QUOTE_MAX_SIZE];
+
+enum { SUCCESS = 0, FAILURE = -1 };
 
 static ssize_t rw_file(const char* path, uint8_t* buf, size_t len, bool do_write) {
     ssize_t bytes = 0;
@@ -67,6 +75,64 @@ static ssize_t rw_file(const char* path, uint8_t* buf, size_t len, bool do_write
 
     close(fd);
     return ret < 0 ? ret : bytes;
+}
+
+static inline int64_t local_sgx_getkey(sgx_key_request_t * keyrequest,
+                                       sgx_key_128bit_t* key)
+{
+    int64_t rax = EGETKEY;
+    __asm__ volatile(
+    ENCLU "\n"
+    : "+a"(rax)
+    : "b"(keyrequest), "c"(key)
+    : "memory");
+    return rax;
+}
+
+static int getkey(sgx_key_128bit_t* key) {
+    ssize_t bytes;
+
+
+    /* 1. write some custom data to `user_report_data` file */
+    sgx_report_data_t user_report_data = {0};
+    uint8_t data[SGX_REPORT_DATA_SIZE];
+
+    /* Test user data */
+    memcpy((uint8_t*) data,
+           "795fa68798a644d32c1d8e2cfe5834f2390e097f0223d94b4758298d1b5501e5", 64);
+
+    memcpy((void*)&user_report_data, (void*)data, sizeof(user_report_data));
+
+    bytes = rw_file("/dev/attestation/user_report_data", (uint8_t*)&user_report_data,
+                         sizeof(user_report_data), /*do_write=*/true);
+    if (bytes != sizeof(user_report_data)) {
+        printf("Test prep user_data failed %d\n", errno);
+        return FAILURE;
+    }
+
+    /* 4. read `report` file */
+    sgx_report_t report;
+    bytes = rw_file("/dev/attestation/report", (uint8_t*)&report, sizeof(report), false);
+    if (bytes != sizeof(report)) {
+        /* error is already printed by file_read_f() */
+        return FAILURE;
+    }
+
+    /* setup key request structure */
+    __sgx_mem_aligned sgx_key_request_t key_request;
+    memset(&key_request, 0, sizeof(key_request));
+    key_request.key_name = SGX_SEAL_KEY;
+    memcpy(&key_request.key_id, &(report.key_id), sizeof(key_request.key_id));
+
+    /* retrieve key via EGETKEY instruction leaf */
+    memset(*key, 0, sizeof(*key));
+    local_sgx_getkey(&key_request, key);
+
+    printf("Got key:\n");
+    print_bytes(sizeof(*key), *key);
+    printf("\n");
+
+    return SUCCESS;
 }
 
 bool Attest(int claims_size, byte* claims, int* size_out, byte* out) {
@@ -105,6 +171,240 @@ bool Attest(int claims_size, byte* claims, int* size_out, byte* out) {
 
     return true;
 }
+
+bool Verify(int user_data_size, byte* user_data, int assertion_size, byte *assertion, int* size_out, byte* out) {
+    ssize_t bytes;
+    int ret = -1;
+
+    printf("Gramine Verify called user_data_size: %d assertion_size: %d\n",
+           user_data_size, assertion_size);
+
+    /* 1. write some custom data to `user_report_data` file */
+    sgx_report_data_t user_report_data = {0};
+
+    /* Get a SHA256 of user_data */
+    mbedtls_sha256(user_data, user_data_size, user_report_data.d, 0);
+
+    bytes = rw_file("/dev/attestation/user_report_data", (uint8_t*)&user_report_data,
+                    sizeof(user_report_data), /*do_write=*/true);
+    if (bytes != sizeof(user_report_data)) {
+        printf("Verify prep user_data failed %d\n", errno);
+        return false;
+    }
+
+    /* 2. read `quote` file */
+    bytes = rw_file("/dev/attestation/quote", (uint8_t*)&g_quote, sizeof(g_quote),
+		    /*do_write=*/false);
+    if (bytes < 0) {
+        printf("Verify quote interface for user_data failed %d\n", errno);
+        return false;
+    }
+
+    sgx_quote_body_t* quote_body_expected = (sgx_quote_body_t*)assertion;
+    sgx_quote_body_t* quote_body_received = (sgx_quote_body_t*)g_quote;
+
+    if (quote_body_expected->version != /*EPID*/2 && quote_body_expected->version != /*DCAP*/3) {
+        printf("version of SGX quote is not EPID (2) and not ECDSA/DCAP (3)\n");
+        return false;
+    }
+
+    /* Compare user report and actual report */
+    printf("Comparing user report data in SGX quote size: %ld\n",
+           sizeof(quote_body_expected->report_body.report_data.d));
+
+    ret = memcmp(quote_body_received->report_body.report_data.d, user_report_data.d,
+                 sizeof(user_report_data));
+    if (ret) {
+        printf("comparison of user report data in SGX quote failed\n");
+        return false;
+    }
+
+    /* Compare expected and actual report */
+    printf("Comparing quote report data in SGX quote size: %ld\n",
+           sizeof(quote_body_expected->report_body.report_data.d));
+
+    ret = memcmp(quote_body_expected->report_body.report_data.d,
+                 quote_body_received->report_body.report_data.d,
+                 sizeof(quote_body_expected->report_body.report_data.d));
+    if (ret) {
+        printf("comparison of quote report data in SGX quote failed\n");
+        return false;
+    }
+
+    printf("\nGramine verify quote interface mr_enclave: ");
+    print_bytes(SGX_QUOTE_SIZE, quote_body_expected->report_body.mr_enclave.m);
+
+    /* Copy out quote info */
+    memcpy(out, quote_body_expected->report_body.mr_enclave.m, SGX_QUOTE_SIZE);
+    *size_out = SGX_QUOTE_SIZE;
+
+    printf("\nGramine verify quote interface compare done, output: \n");
+    print_bytes(*size_out, out);
+    printf("\n");
+
+    return true;
+}
+
+bool Seal(int in_size, byte* in, int* size_out, byte* out) {
+    int ret = 0;
+    bool status = true;
+    __sgx_mem_aligned uint8_t key[KEY_SIZE];
+    uint8_t tag[TAG_SIZE];
+    unsigned char enc_buf[in_size];
+    mbedtls_gcm_context gcm;
+    int tag_size = TAG_SIZE;
+    int i, j = 0;
+
+    printf("Seal: Input size: %d\n", in_size);
+
+    memset(enc_buf, 0, sizeof(enc_buf));
+
+    /* Get SGX Sealing Key */
+    if (getkey(&key) == FAILURE) {
+        printf("getkey failed to retrieve SGX Sealing Key\n");
+	return false;
+    }
+
+    /* Use GCM encrypt/decrypt */
+    mbedtls_gcm_init(&gcm);
+    ret = mbedtls_gcm_setkey(&gcm, MBEDTLS_CIPHER_ID_AES, key, 128);
+
+    if (ret != 0) {
+        printf("mbedtls_gcm_setkey failed: %d\n", ret);
+        status = false;
+	goto done;
+    }
+
+    ret = mbedtls_gcm_crypt_and_tag(&gcm, MBEDTLS_GCM_ENCRYPT, in_size, key, KEY_SIZE,
+		                    NULL, 0, in, enc_buf, TAG_SIZE, tag);
+
+    if (ret != 0) {
+        printf("mbedtls_gcm_crypt_and_tag failed: %d\n", ret);
+        status = false;
+	goto done;
+    }
+
+#ifdef DEBUG
+    printf("Testing seal interface - input buf:\n");
+    print_bytes(in_size, in);
+    printf("\n");
+    printf("Testing seal interface - encrypted buf:\n");
+    print_bytes(sizeof(enc_buf), enc_buf);
+    printf("\n");
+    printf("Testing seal interface - tag:\n");
+    print_bytes(TAG_SIZE, tag);
+    printf("\n");
+#endif
+
+    for (i = 0; i < sizeof(int); i++, j++) {
+        out[j] = ((byte*)&in_size)[i];
+    }
+    for (i = 0; i < TAG_SIZE; i++, j++) {
+        out[j] = tag[i];
+    }
+    for (i = 0; i < sizeof(enc_buf); i++, j++) {
+        out[j] = enc_buf[i];
+    }
+
+    *size_out = j;
+
+#ifdef DEBUG
+    printf("Testing seal interface - out:\n");
+    print_bytes(*size_out, out);
+    printf("\n");
+#endif
+
+    printf("Seal: Successfully sealed size: %d\n", *size_out);
+done:
+    mbedtls_gcm_free(&gcm);
+    return status;
+}
+
+bool Unseal(int in_size, byte* in, int* size_out, byte* out) {
+    int ret = 0;
+    bool status = true;
+    __sgx_mem_aligned uint8_t key[KEY_SIZE];
+    uint8_t tag[TAG_SIZE];
+    mbedtls_gcm_context gcm;
+    int tag_size = TAG_SIZE;
+    int enc_size = 0;
+    int i, j = 0;
+
+    printf("Preparing Unseal size: %d input: \n", in_size);
+    print_bytes(in_size, in);
+    printf("\n");
+
+    for (i = 0; i < sizeof(int); i++, j++) {
+        ((byte*)&enc_size)[i] = in[j];
+    }
+
+    for (i = 0; i < TAG_SIZE; i++, j++) {
+        tag[i] = in[j];
+    }
+
+    unsigned char enc_buf[enc_size];
+    unsigned char dec_buf[enc_size];
+
+    memset(enc_buf, 0, enc_size);
+    memset(dec_buf, 0, enc_size);
+
+    for (i = 0; i < enc_size; i++, j++) {
+        enc_buf[i] = in[j];
+    }
+
+#ifdef DEBUG
+    printf("Testing unseal interface - encrypted buf: size: %d\n", enc_size);
+    print_bytes(enc_size, enc_buf);
+    printf("\n");
+    printf("Testing unseal interface - tag:\n");
+    print_bytes(TAG_SIZE, tag);
+    printf("\n");
+#endif
+
+    /* Get SGX Sealing Key */
+    if (getkey(&key) == FAILURE) {
+        printf("getkey failed to retrieve SGX Sealing Key\n");
+	return false;
+    }
+
+    /* Use GCM encrypt/decrypt */
+    mbedtls_gcm_init(&gcm);
+    ret = mbedtls_gcm_setkey(&gcm, MBEDTLS_CIPHER_ID_AES, key, 128);
+
+    if (ret != 0) {
+        printf("mbedtls_gcm_setkey failed: %d\n", ret);
+	status = false;
+	goto done;
+    }
+
+    /* Invoke unseal */
+    ret = mbedtls_gcm_auth_decrypt(&gcm, enc_size, key, KEY_SIZE, NULL, 0,
+		                   tag, TAG_SIZE, enc_buf, dec_buf);
+    if (ret != 0) {
+        printf("mbedtls_gcm_auth_decrypt failed: %d\n", ret);
+	status = false;
+	goto done;
+    }
+
+#ifdef DEBUG
+    printf("Testing seal interface - decrypted buf:\n");
+    print_bytes(enc_size, dec_buf);
+    printf("\n");
+#endif
+
+    /* Set size */
+    *size_out = enc_size;
+    for (i = 0; i < enc_size; i++) {
+        out[i] = dec_buf[i];
+    }
+
+    printf("Successfully unsealed size: %d\n", *size_out);
+
+done:
+    mbedtls_gcm_free(&gcm);
+    return status;
+}
+
 #if 0
 bool gramine_Attest(int claims_size, byte* claims, int* size_out, byte* out) {
   byte assertion[MAX_ASSERTION_SIZE];
