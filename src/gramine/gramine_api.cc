c6f499846b07f446ddff64ec81e9cc927ff8c6cb
Anoop Jaishankar <ajaishankar@vmware.com>
2023-03-10 14:06:02 -0800

    Cleanup SSL init that's not needed for current test, to be included later

--- src/gramine/gramine_api.cc
+++ src/gramine/gramine_api.cc
@@ -56,13 +56,7 @@ bool gramine_Init(const char *measurement_file, byte *measurement_out) {
   char attestation_type_str[ATTESTATION_TYPE_SIZE] = {0};
   void* ra_tls_attest_lib;
   size_t ret = 0;
-/*
-  mbedtls_ssl_context ssl;
-  mbedtls_ssl_config conf;
 
-  mbedtls_ssl_init(&ssl);
-  mbedtls_ssl_config_init(&conf);
-*/
   ret = rw_file("/dev/attestation/attestation_type", (uint8_t*)attestation_type_str,
                 sizeof(attestation_type_str) - 1, /*do_write=*/false);
   if (ret < 0 && ret != -ENOENT) {
