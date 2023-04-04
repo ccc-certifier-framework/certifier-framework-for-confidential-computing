460d3d8f6472d76f735388717f2f022f324f4083
Anoop Jaishankar <ajaishankar@vmware.com>
2023-04-04 10:14:42 -0700

    Add a few functions for simple_app, to be tested

--- sample_apps/simple_app_under_gramine/gramine_tests.cc
+++ sample_apps/simple_app_under_gramine/gramine_tests.cc
@@ -50,6 +50,7 @@ DEFINE_string(policy_store_file, "store.bin", "policy store file name");
 //    run-app-as-server: This runs the app as a client
 
 #include "policy_key.cc"
+//cc_trust_data* app_trust_data = nullptr;
 
 // -----------------------------------------------------------------------------------------
 
@@ -166,3 +167,159 @@ exit:
 
     return ret;
 }
+
+#if 0
+void client_application(secure_authenticated_channel& channel) {
+
+  printf("Client peer id is %s\n", channel.peer_id_.c_str());
+
+  // client sends a message over authenticated, encrypted channel
+  const char* msg = "Hi from your secret client\n";
+  channel.write(strlen(msg), (byte*)msg);
+
+  // Get server response over authenticated, encrypted channel and print it
+  string out;
+  int n = channel.read(&out);
+  printf("SSL client read: %s\n", out.data());
+}
+
+
+void server_application(secure_authenticated_channel& channel) {
+
+  printf("Server peer id is %s\n", channel.peer_id_.c_str());
+
+  // Read message from client over authenticated, encrypted channel
+  string out;
+  int n = channel.read(&out);
+  printf("SSL server read: %s\n", (const char*) out.data());
+
+  // Reply over authenticated, encrypted channel
+  const char* msg = "Hi from your secret server\n";
+  channel.write(strlen(msg), (byte*)msg);
+}
+
+bool run_me_as_server(const string& host_name, int port,
+      string& asn1_policy_cert, key_message& private_key,
+      string& private_key_cert) {
+
+  printf("running as server\n");
+  server_dispatch(host_name, port, asn1_policy_cert, private_key,
+      private_key_cert, server_application);
+  return true;
+}
+
+int main(int an, char** av) {
+  gflags::ParseCommandLineFlags(&an, &av, true);
+  an = 1;
+  ::testing::InitGoogleTest(&an, av);
+
+  if (FLAGS_operation == "") {
+    printf("sev_example_app.exe --print_all=true|false --operation=op --policy_host=policy-host-address --policy_port=policy-host-port\n");
+    printf("\t --data_dir=-directory-for-app-data --server_app_host=my-server-host-address --server_app_port=server-host-port\n");
+    printf("\t --policy_cert_file=self-signed-policy-cert-file-name --policy_store_file=policy-store-file-name\n");
+    printf("\t --ark_cert_file=./service/milan_ark_cert.der --ask_cert_file=./service/milan_ask_cert.der --vcek_cert_file=./service/milan_vcek_cert.der\n");
+    printf("Operations are: cold-init, warm-restart, get-certifier, run-app-as-client, run-app-as-server\n");
+    return 0;
+  }
+
+  SSL_library_init();
+  string enclave_type("sev-enclave");
+  string purpose("authentication");
+
+  string store_file(FLAGS_data_dir);
+  store_file.append(FLAGS_policy_store_file);
+  app_trust_data = new cc_trust_data(enclave_type, purpose, store_file);
+  if (app_trust_data == nullptr) {
+    printf("couldn't initialize trust object\n");
+    return 1;
+  }
+
+  // Init policy key info
+  if (!app_trust_data->init_policy_key(initialized_cert_size, initialized_cert)) {
+    printf("Can't init policy key\n");
+    return 1;
+  }
+
+  // Init sev enclave
+  if (!app_trust_data->initialize_sev_enclave_data(FLAGS_ark_cert_file,
+        FLAGS_ask_cert_file, FLAGS_vcek_cert_file)) {
+    printf("Can't init sev enclave\n");
+    return 1;
+  }
+
+  // Standard algorithms for the enclave
+  string public_key_alg("rsa-2048");
+  string symmetric_key_alg("aes-256");
+  string hash_alg("sha-256");
+  string hmac_alg("sha-256-hmac");
+
+  // Carry out operation
+  int ret = 0;
+  if (FLAGS_operation == "cold-init") {
+    if (!app_trust_data->cold_init(public_key_alg,
+        symmetric_key_alg, hash_alg, hmac_alg)) {
+      printf("cold-init failed\n");
+      ret = 1;
+    }
+  } else if (FLAGS_operation == "warm-restart") {
+    if (!app_trust_data->warm_restart()) {
+      printf("warm-restart failed\n");
+      ret = 1;
+    }
+
+  } else if (FLAGS_operation == "get-certifier") {
+    if (!app_trust_data->certify_me(FLAGS_policy_host, FLAGS_policy_port)) {
+      printf("certification failed\n");
+      ret = 1;
+    }
+  } else if (FLAGS_operation == "run-app-as-client") {
+    if (!app_trust_data->warm_restart()) {
+      printf("warm-restart failed\n");
+      ret = 1;
+      goto done;
+    }
+    printf("running as client\n");
+    if (!app_trust_data->cc_auth_key_initialized_ ||
+        !app_trust_data->cc_policy_info_initialized_) {
+      printf("trust data not initialized\n");
+      ret = 1;
+      goto done;
+    }
+    string my_role("client");
+    secure_authenticated_channel channel(my_role);
+    if (!channel.init_client_ssl(FLAGS_server_app_host, FLAGS_server_app_port,
+          app_trust_data->serialized_policy_cert_,
+          app_trust_data->private_auth_key_,
+          app_trust_data->private_auth_key_.certificate())) {
+      printf("Can't init client app\n");
+      ret = 1;
+      goto done;
+    }
+
+  // This is the actual application code.
+  client_application(channel);
+  } else if (FLAGS_operation == "run-app-as-server") {
+    if (!app_trust_data->warm_restart()) {
+      printf("warm-restart failed\n");
+      ret = 1;
+      goto done;
+    }
+    printf("running as server\n");
+    server_dispatch(FLAGS_server_app_host, FLAGS_server_app_port,
+        app_trust_data->serialized_policy_cert_,
+          app_trust_data->private_auth_key_,
+          app_trust_data->private_auth_key_.certificate(),
+          server_application);
+  } else {
+    printf("Unknown operation\n");
+  }
+
+done:
+  // app_trust_data->print_trust_data();
+  app_trust_data->clear_sensitive_data();
+  if (app_trust_data != nullptr) {
+    delete app_trust_data;
+  }
+  return ret;
+}
+#endif
