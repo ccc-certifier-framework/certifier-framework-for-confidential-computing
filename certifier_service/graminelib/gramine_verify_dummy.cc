e88bfcd9b5da2492b405cf5250a515a4d167344f
Ye Li <yel@vmware.com>
2023-04-05 17:36:31 -0400

    Gramine Go wrapper test patch.

---
+++ certifier_service/graminelib/gramine_verify_dummy.cc
@@ -0,0 +1,7 @@
+#include "gramineverify.h"
+
+bool gramine_Verify(const int what_to_say_size, byte* what_to_say,
+    const int attestation_size, byte* attestation,
+    int* measurement_out_size, byte* measurement_out) {
+    return false;
+}
