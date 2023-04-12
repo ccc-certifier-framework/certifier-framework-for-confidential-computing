198992776622e2acafcd546823cef1ad9db5d310
Anoop Jaishankar <ajaishankar@vmware.com>
2023-04-12 10:23:56 -0700

    Add copyright to service files

--- certifier_service/graminelib/gramine_verify_dummy.cc
+++ certifier_service/graminelib/gramine_verify_dummy.cc
@@ -1,3 +1,17 @@
+//  Copyright (c) 2022-23, VMware Inc, and the Certifier Authors.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "gramineverify.h"
 
 bool gramine_Verify(const int what_to_say_size, byte* what_to_say,
