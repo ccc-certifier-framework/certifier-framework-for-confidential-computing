6682351b0f6639294e73d01dde18aeb18478a572
Anoop Jaishankar <ajaishankar@vmware.com>
2023-04-12 10:36:14 -0700

    Update to correct verification function

--- certifier_service/graminelib/gramine_verify.cc
+++ certifier_service/graminelib/gramine_verify.cc
@@ -14,14 +14,14 @@
 
 #include "gramine_verify.h"
 
-extern bool gramine_remote_verify_impl(const int what_to_say_size, byte* what_to_say, const int attestation_size, byte* attestation, int* measurement_out_size, byte* measurement_out);
+extern bool gramine_remote_verify(const int what_to_say_size, byte* what_to_say, const int attestation_size, byte* attestation, int* measurement_out_size, byte* measurement_out);
 
 bool gramine_Verify(const int what_to_say_size, byte* what_to_say,
     const int attestation_size, byte* attestation,
     int* measurement_out_size, byte* measurement_out) {
   bool result = false;
 
-  result = gramine_remote_verify_impl
+  result = gramine_remote_verify
            (what_to_say_size, what_to_say, attestation_size,
             attestation, measurement_out_size, measurement_out);
   if (!result) {
