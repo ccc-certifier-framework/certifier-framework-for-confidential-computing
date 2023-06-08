50c1a1afae38740f14ce12c87d46cff9751011a8
Sangwan Kwon <sangwan.kwon@samsung.com>
2023-06-09 08:09:52 +0900

    Add CCA shim layer (#101)
    $ make shim_test
    
    What was said: User Custom data
    Measurement: 6190EB90B293886C172EC644DAFB7E33EE2CEA6541ABE15300D96380DF525BF9
    Attestation test succeeded.
    Success sealing round trip.
    Sealing test succeeded.
    
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

---
+++ src/cca/cca.h
@@ -0,0 +1,34 @@
+//  Copyright (c) 2021-22, VMware Inc, and the Certifier Authors.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <string.h>
+
+#ifndef byte
+typedef unsigned char byte;
+#endif
+
+bool cca_Init(const int cert_size, byte *cert);
+bool cca_Attest(const int what_to_say_size, byte* what_to_say, int* attestation_size_out, byte* attestation_out);
+bool cca_Verify(const int what_to_say_size, byte* what_to_say, const int attestation_size, byte* attestation, int* measurement_out_size, byte* measurement_out);
+bool cca_Seal(int in_size, byte* in, int* size_out, byte* out);
+bool cca_Unseal(int in_size, byte* in, int* size_out, byte* out);
