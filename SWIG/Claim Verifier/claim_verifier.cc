de7fc2aa44996e158d975c98b650bfdfaeb38226
Pari Patel <117529246+paritheplatypus@users.noreply.github.com>
2025-08-14 14:12:26 -0500

    Create claim_verifier.cc

---
+++ SWIG/Claim Verifier/claim_verifier.cc
@@ -0,0 +1,18 @@
+#include "claim_verifier.h"
+#include "certifier.pb.h"
+#include "support.h"
+
+ClaimVerifier::ClaimVerifier() {}
+
+ClaimVerifier::~ClaimVerifier() {}
+
+bool ClaimVerifier::verify(const std::string& serialized_claim, const std::string& serialized_key) {
+    signed_claim_message claim;
+    key_message key;
+
+    if (!claim.ParseFromString(serialized_claim) || !key.ParseFromString(serialized_key)) {
+        return false;
+    }
+
+    return verify_signed_claim(claim, key);
+}
