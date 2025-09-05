c548c837167784d58d8121321da71df61461a11a
John Manferdelli <jlmucbmath@gmail.com>
2025-09-05 11:51:11 -0700

    start of sev port to new API (#293)

--- sample_apps/simple_app_under_sev/prepare-test.sh
+++ sample_apps/simple_app_under_sev/prepare-test.sh
@@ -50,6 +50,8 @@ echo "policy cert file name: $POLICY_CERT_FILE_NAME"
 POLICY_STORE_NAME="policy_store.$DOMAIN_NAME"
 echo "policy store name: $POLICY_STORE_NAME"
 
+SIMULATED_SEV=1
+
 function do-fresh() {
   echo "do-fresh"
 
@@ -134,6 +136,29 @@ function do-fresh() {
   exit
 }
 
+function initialize-sev-simulator() {
+  echo "initialize-sev-simulator"
+
+  pushd $CERTIFIER_ROOT/sev-snp-simulator
+    if [[ -d "/etc/certifier-snp-sim" ]] ; then
+      if [[ -e "ec-secp384r1-priv-key.pem" && -e ec-secp384r1-pub-key.pem ]] ; then
+        echo "Sev simultator keys already exist"
+        return
+      fi
+    fi
+    make
+    make keys
+    if [[ ! -d /etc/certifier-snp-sim ]] ; then
+      sudo etc/certifier-snp-sim
+    fi
+    set +e
+    sudo make insmod
+    sudo cp ./keys/* /etc/certifier-snp-sim
+    set -e
+  popd
+  echo "initialize-sev-simulator done"
+}
+
 function do-compile-utilities() {
   echo "do-compile-utilities"
 
@@ -157,8 +182,11 @@ function do-make-keys() {
       --operation=generate-policy-key-and-test-keys  \
       --policy_key_output_file=$POLICY_KEY_FILE_NAME  \
       --policy_cert_output_file=$POLICY_CERT_FILE_NAME \
-      --platform_key_output_file=platform_key_file.bin  \
-      --attest_key_output_file=attest_key_file.bin
+
+    $CERTIFIER_PROTOTYPE/utilities/simulated_sev_key_generation.exe  \
+         --ark_der=ark_cert.der --ask_der=ask_cert.der \
+         --vcek_der=vcek_cert.der  \
+         --vcek_key_file=/etc/certifier-snp-sim/ec-secp384r1-pub-key.pem
   popd
 
   echo "do-make-keys done"
@@ -173,32 +201,59 @@ function do-compile-program() {
       --input=$POLICY_CERT_FILE_NAME --output=../policy_key.cc
     popd
 
-    make -f example_app.mak
+    if [[ -v SIMULATED_SEV ]] ; then
+      CFLAGS='-DSEV_DUMMY_GUEST' make -f sev_example_app.mak
+    else
+      make -f sev_example_app.mak
+    fi
   popd
 
   echo "do-compile-program done"
 }
 
+# usage: sev-snp-measure [-h] [--version] [-v] --mode {sev,seves,snp,snp:ovmf-hash,snp:svsm}
+#       [--vcpus N] [--vcpu-type CPUTYPE] [--vcpu-sig VALUE]
+#       [--vcpu-family FAMILY] [--vcpu-model MODEL] [--vcpu-stepping STEPPING]
+#       [--vmm-type VMMTYPE] --ovmf PATH [--kernel PATH] [--initrd PATH]
+#       [--append CMDLINE] [--guest-features VALUE]
+#       [--output-format {hex,base64}] [--snp-ovmf-hash HASH] [--dump-vmsa]
+#       [--svsm PATH] [--vars-size SIZE | --vars-file PATH]
+# The following arguments are required: --mode, --ovmf
+
 function do-make-policy() {
   echo "do-make-policy"
 
   pushd $EXAMPLE_DIR/provisioning
-
     echo " "
 
-    $CERTIFIER_ROOT/utilities/measurement_utility.exe      \
-      --type=hash --input=../example_app.exe --output=example_app.measurement
+    if [[ -v SIMULATED_SEV ]] ; then
+      $CERTIFIER_ROOT/utilities/measurement_utility.exe      \
+        --mrenclave=010203040506070801020304050607080102030405060708010203040506070801020304050607080102030405060708\
+        --input=../example_app.exe --output=sev-example-app.measurement
+    else
+      pushd $CERTIFIER_ROOT
+        if [[ ! -d "../sev-snp-measure" ]] ; then
+          echo "sev-snp-measure tool not found"
+          exit
+        else
+          snp-tool-dir=$(pwd)
+          snp-tool="$snp-tool-dir/sev-snp-measure.py"
+          echo "sev-snp-measure arguments not ready"
+          exit
+        fi
+      popd
+    fi
 
-    $CERTIFIER_ROOT/utilities/make_unary_vse_clause.exe \
-      --key_subject="platform_key_file.bin" --verb="is-trusted-for-attestation" --output=ts1.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_unary_vse_clause.exe    \
+        --certificate-subject=ark_cert.der --verb="is-trusted-for-attestation"  \
+        --output=ts1.bin
     $CERTIFIER_ROOT/utilities/make_indirect_vse_clause.exe \
       --key_subject=$POLICY_KEY_FILE_NAME --verb="says" \
       --clause=ts1.bin --output=vse_policy1.bin
 
     $CERTIFIER_ROOT/utilities/make_unary_vse_clause.exe \
-      --key_subject="" --measurement_subject="example_app.measurement" \
+      --key_subject="" --measurement_subject="sev-example-app.measurement" \
       --verb="is-trusted" --output=ts2.bin
-
     $CERTIFIER_ROOT/utilities/make_indirect_vse_clause.exe \
       --key_subject=$POLICY_KEY_FILE_NAME --verb="says" \
       --clause=ts2.bin --output=vse_policy2.bin
@@ -206,29 +261,79 @@ function do-make-policy() {
     $CERTIFIER_ROOT/utilities/make_signed_claim_from_vse_clause.exe \
       --vse_file=vse_policy1.bin --duration=9000 --private_key_file=$POLICY_KEY_FILE_NAME \
       --output=signed_claim_1.bin
-
     $CERTIFIER_ROOT/utilities/make_signed_claim_from_vse_clause.exe \
       --vse_file=vse_policy2.bin  --duration=9000  \
       --private_key_file=$POLICY_KEY_FILE_NAME --output=signed_claim_2.bin
 
-    $CERTIFIER_ROOT/utilities/package_claims.exe \
-      --input=signed_claim_1.bin,signed_claim_2.bin --output=policy.bin
-    $CERTIFIER_ROOT/utilities/print_packaged_claims.exe --input=policy.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_property.exe    \
+        --property_name=debug                       \
+        --property_type='string' comparator="="     \
+        --string_value=no                           \
+        --output=property1.bin
 
-    $CERTIFIER_ROOT/utilities/make_unary_vse_clause.exe \
-      --key_subject=attest_key_file.bin --verb="is-trusted-for-attestation" --output=tsc1.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_property.exe    \
+        --property_name=migrate                     \
+        --property_type='string' comparator="="     \
+        --string_value=no                           \
+        --output=property2.bin
 
-    $CERTIFIER_ROOT/utilities/make_indirect_vse_clause.exe \
-      --key_subject=platform_key_file.bin --verb="says" \
-      --clause=tsc1.bin --output=vse_policy3.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_property.exe    \
+        --property_name=smt                         \
+        --property_type='string' comparator="="     \
+        --string_value=no                           \
+        --output=property5.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_property.exe    \
+        --property_name='api-major'                 \
+        --property_type=int                         \
+        --comparator=">="                           \
+        --int_value=0                               \
+        --output=property3.bin
 
-    $CERTIFIER_ROOT/utilities/make_signed_claim_from_vse_clause.exe \
-      --vse_file=vse_policy3.bin --duration=9000 \
-      --private_key_file=platform_key_file.bin --output=platform_attest_endorsement.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_property.exe    \
+        --property_name='api-minor'                 \
+        --property_type=int                         \
+        --comparator=">="                           \
+        --int_value=0                               \
+        --output=property4.bin
 
-    echo " "
+    $CERTIFIER_PROTOTYPE/utilities/make_property.exe    \
+        --property_name='tcb-version'               \
+        --property_type=int                         \
+        --comparator="="                            \
+        --int_value=0x03000000000008115             \
+        --output=property6.bin
+
+    $CERTIFIER_PROTOTYPE/utilities/combine_properties.exe   \
+      --in=property1.bin,property2.bin,property3.bin,property4.bin,property5.bin,property6.bin \
+      --output=properties.bin
+
+    $CERTIFIER_PROTOTYPE/utilities/make_platform.exe    \
+        --platform_type=amd-sev-snp                 \
+        --properties_file=properties.bin            \
+        --output=platform.bin
+
+    $CERTIFIER_PROTOTYPE/utilities/make_unary_vse_clause.exe    \
+        --platform_subject=platform.bin                     \
+        --verb="has-trusted-platform-property"              \
+        --output=ts3.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_indirect_vse_clause.exe     \
+        --key_subject=policy_key_file.bin                       \
+        --verb="says"                                           \
+        --clause=ts3.bin                                        \
+        --output=vse_policy3.bin
+    $CERTIFIER_PROTOTYPE/utilities/make_signed_claim_from_vse_clause.exe    \
+        --vse_file=vse_policy3.bin                                      \
+        --duration=9000                                                 \
+        --private_key_file=policy_key_file.bin                          \
+        --output=signed_claim_3.bin
+
+    $CERTIFIER_PROTOTYPE/utilities/package_claims.exe                           \
+        --input=signed_claim_1.bin,signed_claim_2.bin,signed_claim_3.bin    \
+        --output=policy.bin
+    $CERTIFIER_PROTOTYPE/utilities/print_packaged_claims.exe --input=policy.bin
   popd
 
+  echo " "
   echo "do-make-policy done"
 }
 
@@ -297,6 +402,7 @@ function do-all() {
   echo " "
   echo "do-all"
 
+  do-initialize-sev-simulator
   do-compile-utilities
   do-make-keys
   do-compile-program
