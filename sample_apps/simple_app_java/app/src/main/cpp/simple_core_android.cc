4812404da4dcb967b365294a9d916ee8a27bcffc
Pari Patel <117529246+paritheplatypus@users.noreply.github.com>
2025-09-18 13:55:15 -0500

    Update simple_core_android.cc

--- sample_apps/simple_app_java/app/src/main/cpp/simple_core_android.cc
+++ sample_apps/simple_app_java/app/src/main/cpp/simple_core_android.cc
@@ -5,7 +5,9 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,  "CertifierJNI", __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "CertifierJNI", __VA_ARGS__)
 
-// Later: #include "certifier/..." and call TrustManager, channels, etc.
+// Later you'll include Certifier headers and call its API.
+// #include "..."  // from your local copy or from CERTIFIER_ROOT/include
+
 std::string run_certifier_simple(const std::string& workDir,
                                  const std::string& mode,
                                  const std::string& host,
@@ -13,8 +15,7 @@ std::string run_certifier_simple(const std::string& workDir,
     LOGI("run_certifier_simple(workDir=%s, mode=%s, host=%s, port=%d)",
          workDir.c_str(), mode.c_str(), host.c_str(), port);
 
-    // TODO: read policy/keys from workDir, init trust manager, eval policy,
-    // and (optionally) connect to host:port.
+    // TODO: Copy assets to workDir (policy/keys), then call TrustManager, etc.
     std::ostringstream out;
     out << "Certifier native OK\n"
         << "mode=" << mode << " host=" << host << " port=" << port << "\n"
