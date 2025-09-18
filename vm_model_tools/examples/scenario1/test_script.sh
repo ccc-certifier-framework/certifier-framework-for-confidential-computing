da25b264e8df2c1d0c63c60d63ebc809acab7912
John Manferdelli <johnmanferdelli@hotmail.com>
2025-09-17 17:09:40 -0700

    Fixed script

--- vm_model_tools/examples/scenario1/test_script.sh
+++ vm_model_tools/examples/scenario1/test_script.sh
@@ -4,25 +4,46 @@
 # This can only be run after the certification is ./run-test.sh.
 # ############################################################################
 
+pushd ../../..
+  CERTIFIER_ROOT=$(pwd)
+popd
+EXAMPLE_DIR=$(pwd)
 
-../../src/cf_utility.exe --print_cryptstore=true --policy_domain_name=dom0 \
+echo " "
+echo "CERTIFIER_ROOT: $CERTIFIER_ROOT"
+echo "EXAMPLE_DIR: $EXAMPLE_DIR"
+echo " "
+
+echo " "
+echo "printing cryptstore"
+echo " "
+$CERTIFIER_ROOT/vm_model_tools/src/cf_utility.exe --print_cryptstore=true --policy_domain_name=dom0 \
     --encrypted_cryptstore_filename=cryptstore.dom0 --policy_store_filename=policy_store.dom0 \
     --enclave_type=simulated-enclave \
     --policy_key_cert_file=policy_cert_file.dom0 --data_dir=./
 
-../../src/cf_utility.exe --export_cryptstore=true --policy_domain_name=dom0 \
+echo " "
+echo "exporting cryptstore"
+echo " "
+$CERTIFIER_ROOT/vm_model_tools/src/cf_utility.exe --export_cryptstore=true --policy_domain_name=dom0 \
     --encrypted_cryptstore_filename=cryptstore.dom0 --output_file=exported.cryptstore \
     --policy_store_filename=policy_store.dom0 --enclave_type=simulated-enclave \
     --policy_key_cert_file=policy_cert_file.dom0 --data_dir=./
 
-../../src/cf_key_server.exe --policy_domain_name=dom0 \
+echo " "
+echo "running key-server"
+echo " "
+$CERTIFIER_ROOT/vm_model_tools/src/cf_key_server.exe --policy_domain_name=dom0 \
     --encrypted_cryptstore_filename=cryptstore.dom0 \
     --enclave_type=simulated-enclave --policy_store_filename=policy_store.dom0 \
     --policy_key_cert_file=policy_cert_file.dom0 --data_dir=./ &
 
 sleep 5
 
-../../src/cf_key_client.exe --policy_domain_name=dom0 \
+echo " "
+echo "key-client: storing new value"
+echo " "
+$CERTIFIER_ROOT/vm_model_tools/src/cf_key_client.exe --policy_domain_name=dom0 \
     --encrypted_cryptstore_filename=cryptstore.dom0 \
     --enclave_type=simulated-enclave --policy_store_filename=policy_store.dom0 \
     --policy_key_cert_file=policy_cert_file.dom0 --data_dir=./ \
@@ -30,7 +51,10 @@ sleep 5
     --input_format=raw --output_format=raw \
     --input_file=client.in --output_file=client.out --action=store
 
-../../src/cf_key_client.exe --policy_domain_name=dom0 \
+echo " "
+echo "key-client: retrieving"
+echo " "
+$CERTIFIER_ROOT/vm_model_tools/src/cf_key_client.exe --policy_domain_name=dom0 \
     --encrypted_cryptstore_filename=cryptstore.dom0 \
     --enclave_type=simulated-enclave --policy_store_filename=policy_store.dom0 \
     --policy_key_cert_file=policy_cert_file.dom0 --data_dir=./ \
@@ -40,7 +64,10 @@ sleep 5
 
 sleep 3
 
-../../src/cf_utility.exe --print_cryptstore=true --policy_domain_name=dom0 \
+echo " "
+echo "printing cryptstore"
+echo " "
+$CERTIFIER_ROOT/vm_model_tools/src/cf_utility.exe --print_cryptstore=true --policy_domain_name=dom0 \
     --encrypted_cryptstore_filename=cryptstore.dom0 --policy_store_filename=policy_store.dom0 \
     --enclave_type=simulated-enclave \
     --policy_key_cert_file=policy_cert_file.dom0 --data_dir=./
