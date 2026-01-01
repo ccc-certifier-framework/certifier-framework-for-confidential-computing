ef466d751d0836cf260e0e375b8ac1539c513cf1
John Manferdelli <johnmanferdelli@hotmail.com>
2026-01-01 15:19:22 -0800

    formatting

--- vm_model_tools/examples/scenario1/copy-vm-files.sh
+++ vm_model_tools/examples/scenario1/copy-vm-files.sh
@@ -224,35 +224,36 @@ function process-args() {
 
 # ------------------------------------------------------------------------------------------
 
-function do-copy-files() {
-  echo " "
-  echo "do-copy-files"
+function do-copy-vm-files() {
+	echo " "
+	echo "do-copy-vm-files"
       
-  if [[ ! -e "$EXAMPLE_DIR/provisioning" ]] ; then
-    mkdir $EXAMPLE_DIR/provisioning
-  fi
-  if [[ ! -e "$EXAMPLE_DIR/service" ]] ; then
-    mkdir $EXAMPLE_DIR/service
-  fi
-  if [[ ! -e "$EXAMPLE_DIR/cf_data" ]] ; then
-    mkdir $EXAMPLE_DIR/cf_data
-  fi
-  pushd $EXAMPLE_DIR/provisioning
-    cp -p $POLICY_KEY_FILE_NAME $POLICY_CERT_FILE_NAME policy.bin $EXAMPLE_DIR/service
-    cp -p sev_policy.bin ark_cert.der ask_cert.der vcek_cert.der $EXAMPLE_DIR/service
-    cp -p $POLICY_CERT_FILE_NAME $EXAMPLE_DIR/cf_data
-    cp -p platform_key_file.bin attest_key_file.bin sev_cf_utility.measurement $EXAMPLE_DIR/cf_data
-    cp -p cf_utility.measurement platform_attest_endorsement.bin $EXAMPLE_DIR/cf_data
-    cp -p ark_cert.der ask_cert.der vcek_cert.der $EXAMPLE_DIR/cf_data
-  popd
+	if [[ ! -e "$EXAMPLE_DIR/provisioning" ]] ; then
+		mkdir $EXAMPLE_DIR/provisioning
+	fi
+	if [[ ! -e "$EXAMPLE_DIR/service" ]] ; then
+		mkdir $EXAMPLE_DIR/service
+	fi
+	if [[ ! -e "$EXAMPLE_DIR/cf_data" ]] ; then
+		mkdir $EXAMPLE_DIR/cf_data
+	fi
+	pushd $EXAMPLE_DIR/provisioning
+	cp -p $POLICY_KEY_FILE_NAME $POLICY_CERT_FILE_NAME policy.bin $EXAMPLE_DIR/service
+	cp -p sev_policy.bin ark_cert.der ask_cert.der vcek_cert.der $EXAMPLE_DIR/service
+	cp -p $POLICY_CERT_FILE_NAME $EXAMPLE_DIR/cf_data
+	cp -p platform_key_file.bin attest_key_file.bin sev_cf_utility.measurement $EXAMPLE_DIR/cf_data
+	cp -p cf_utility.measurement platform_attest_endorsement.bin $EXAMPLE_DIR/cf_data
+	cp -p ark_cert.der ask_cert.der vcek_cert.der $EXAMPLE_DIR/cf_data
+	popd
 
-  pushd $EXAMPLE_DIR/cf_data
-    $CERTIFIER_ROOT/utilities/combine_policy_certs.exe \
-      --init=true --new_cert_file=$POLICY_CERT_FILE_NAME \
-      --output=my_certs
-  popd
+	pushd $EXAMPLE_DIR/cf_data
+	$CERTIFIER_ROOT/utilities/combine_policy_certs.exe \
+	  --init=true --new_cert_file=$POLICY_CERT_FILE_NAME \
+	  --output=my_certs
+	popd
 
-  echo "do-copy-files done"
+	echo "do-copy-vm-files done"
+	echo " "
 }
 
 # --------------------------------------------------------------------------------------------------
