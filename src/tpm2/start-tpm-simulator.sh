4c9c2724a89cfbdfdb093c5dfbebf0bcdaea9260
John Manferdelli <johnmanferdelli@hotmail.com>
2026-06-15 12:43:02 -0700

    shell fix

--- src/tpm2/start-tpm-simulator.sh
+++ src/tpm2/start-tpm-simulator.sh
@@ -40,12 +40,16 @@ pushd $TPM_SUPPORT_DIR
   swtpm_setup --tpmstate ${XDG_CONFIG_HOME}/mytpm1 --create-ek-cert \
     --create-platform-cert --tpm2 --write-ek-cert-files .
 
+  set +e
   modprobe tpm_vtpm_proxy
   if [[ $? -eq 0 ]] ; then
+    echo "Using chardev"
     swtpm chardev --vtpm-proxy --tpmstate dir=${XDG_CONFIG_HOME}/mytpm1 \
       --tpm2 --ctrl type=tcp,port=2322 --flags not-need-init,startup-clear &
   else
-    swtpm socket --tpmstate dir=${XDG_CONFIG_HOME}/mytpm1--tpm2 --ctrl type=tcp,port=2322 --server type=tcp,port=2321 --flags not-need-init,startup-clear --log level=20 &
+    echo "chardev unavailable, using socket"
+    swtpm socket --tpmstate dir=${XDG_CONFIG_HOME}/mytpm1 --tpm2 --ctrl type=tcp,port=2322 --server type=tcp,port=2321 --flags not-need-init,startup-clear --log level=20 &
+    sleep 2
     socat PTY,link=/dev/tpmrm1,raw,echo=0 TCP4:127.0.0.1:2321 &
   fi
   chmod 0777 *.crt || true
